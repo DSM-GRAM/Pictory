bbc2be604871c86e3c7e794c18fcdf65e129223e
jinusong <sugwer7464@gmail.com>
2019-03-18 23:44:08 +0900

    [ADD] Project

---
+++ app/src/main/java/com/gram/pictory/Activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gram.pictory.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.gram.pictory.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
