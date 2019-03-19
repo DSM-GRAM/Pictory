0f9d479a561963df627c1997c51622e5f1577d32
jinusong <sugwer7464@gmail.com>
2019-03-19 23:12:00 +0900

    [Update] SignUp and API

---
+++ app/src/main/java/com/gram/pictory/Activity/SignUpActivity.kt
@@ -0,0 +1,29 @@
+package com.gram.pictory.Activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.gram.pictory.Connect.Connecter.api
+import kotlinx.android.synthetic.main.activity_signup.*
+import org.jetbrains.anko.toast
+
+class SignUpActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        signUpBtn.setOnClickListener {
+            if (idSignUpEditText.text.isNullOrBlank() || pwSignUpEditText.text.isNullOrBlank()
+                || nameEditText.text.isNullOrBlank() || birthEditText.text.isNullOrBlank()) {
+                toast("정보를 모두 입력해주세요.").show()
+            } else {
+                api.signUp(hashMapOf(
+                    "id" to idSignUpEditText.text,
+                    "password" to pwSignUpEditText.text,
+                    "name" to nameEditText.text,
+                    "birth" to birthEditText.text
+                ))
+                toast("회원가입에 성공했습니다.").show()
+                finish()
+            }
+        }
+    }
+}
