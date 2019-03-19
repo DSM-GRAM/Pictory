f6a955eb3dc58fdca5237cf54e4a9530c33cb662
김영래 <rayray0219@naver.com>
2019-03-19 15:41:07 +0900

    LoginActivity -> MainActivity

---
+++ app/src/main/java/com/gram/pictory/Activity/Fragment/FeedFragment.kt
@@ -0,0 +1,15 @@
+package com.gram.pictory.Activity.Fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gram.pictory.R
+
+class FeedFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_feed, null)
+        return view
+    }
+}
